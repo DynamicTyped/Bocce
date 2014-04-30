e756c3f20bea71225af87ff237c11ba1ead99562
jcoutch <joseph.coutcher@gmail.com>
2014-04-30 11:57:37 -0400

    Bug fix in tracing, and removed a .nuspec file that wasn't used

--- Bocce/Notifications/DiagnosticTrace.cs
+++ Bocce/Notifications/DiagnosticTrace.cs
@@ -7,46 +7,46 @@ using Bocce.Diagnostics;
 
 namespace Bocce.Notifications
 {
-    internal class DiagnosticTrace : IDiagnosticTrace
-    {
-        private static readonly TraceSource TraceSource = new TraceSource("Bocce");
-        internal readonly ConcurrentDictionary<string, Tuple<string, string, string>> _cache = new ConcurrentDictionary<string, Tuple<string, string, string>>();
-        
-        public void ResourceMiss(Resource resource)
-        {
-            Trace(1001, TraceEventType.Error, "OnResourceMiss", string.Format("Resource Missed: {0} - {1} - {2}", resource.ResourceType, resource.Culture.Name, resource.ResourceKey), resource);
-        }
+	internal class DiagnosticTrace : IDiagnosticTrace
+	{
+		private static readonly TraceSource TraceSource = new TraceSource("Bocce");
+		internal readonly ConcurrentDictionary<Tuple<string, string, string>, string> _cache = new ConcurrentDictionary<Tuple<string, string, string>, string>();
 
-        public void ResourceFellBack(Resource resource)
-        {
-            Trace(1002, TraceEventType.Warning, "OnResourceFellBack", string.Format("Resource Fell Back: {0} - {1} - {2}", resource.ResourceType, resource.Culture.Name, resource.ResourceKey), resource);
-        }
+		public void ResourceMiss(Resource resource)
+		{
+			Trace(1001, TraceEventType.Error, "OnResourceMiss", string.Format("Resource Missed: {0} - {1} - {2}", resource.ResourceType, resource.Culture.Name, resource.ResourceKey), resource);
+		}
 
-        public void ResourceCleared(string resourceType)
-        {
-            ClearCache(resourceType);
-            Trace(1003, TraceEventType.Information, "OnResourceCleared", string.Format("Resources cleared for {0}",resourceType),new Resource(){ResourceType = resourceType});
-        }
+		public void ResourceFellBack(Resource resource)
+		{
+			Trace(1002, TraceEventType.Warning, "OnResourceFellBack", string.Format("Resource Fell Back: {0} - {1} - {2}", resource.ResourceType, resource.Culture.Name, resource.ResourceKey), resource);
+		}
 
-        public void ResourceHit(Resource resource)
-        {
-            if (DbResourceProviderSection.GetSection().TraceMatches)
-                Trace(1004, TraceEventType.Verbose, "OnResourceHit", string.Format("Resource Match found: {0} - {1} - {2}", resource.ResourceType, resource.Culture.Name, resource.ResourceKey), resource);
-        }
+		public void ResourceCleared(string resourceType)
+		{
+			ClearCache(resourceType);
+			Trace(1003, TraceEventType.Information, "OnResourceCleared", string.Format("Resources cleared for {0}", resourceType), new Resource() { ResourceType = resourceType });
+		}
 
-        private void Trace(int id, TraceEventType type, string identifier, string description, Resource resource)
-        {
-            if (_cache.TryAdd(identifier, new Tuple<string, string, string>(resource.ResourceType, resource.Culture.Name, resource.ResourceKey)))
-                TraceSource.TraceEvent(id, new ResourceTraceRecord(type, identifier, description){Resource = resource});
-        }
+		public void ResourceHit(Resource resource)
+		{
+			if (DbResourceProviderSection.GetSection().TraceMatches)
+				Trace(1004, TraceEventType.Verbose, "OnResourceHit", string.Format("Resource Match found: {0} - {1} - {2}", resource.ResourceType, resource.Culture.Name, resource.ResourceKey), resource);
+		}
 
-        private void ClearCache(string resourceType)
-        {
-            foreach (var item in _cache.Where(a => string.Equals(a.Value.Item1, resourceType, StringComparison.InvariantCultureIgnoreCase)))
-            {
-                Tuple<string, string, string> resource;
-                _cache.TryRemove(item.Key, out resource);
-            }
-        }
-    }
+		private void Trace(int id, TraceEventType type, string identifier, string description, Resource resource)
+		{
+			if (_cache.TryAdd(new Tuple<string, string, string>(resource.ResourceType, resource.Culture.Name, resource.ResourceKey), identifier))
+				TraceSource.TraceEvent(id, new ResourceTraceRecord(type, identifier, description) { Resource = resource });
+		}
+
+		private void ClearCache(string resourceType)
+		{
+			foreach (var item in _cache.Where(a => string.Equals(a.Key.Item1, resourceType, StringComparison.InvariantCultureIgnoreCase)))
+			{
+				string resource;
+				_cache.TryRemove(item.Key, out resource);
+			}
+		}
+	}
 }
